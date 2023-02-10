56a290509cbee8f5eeb8d790f3249c0ffff8c9ac
Márk Ángyán <angyanmark97@gmail.com>
2023-02-10 23:04:49 +0100

    remove serializer options

--- src/ScratchcardStatistics/Services/LoadService.cs
+++ src/ScratchcardStatistics/Services/LoadService.cs
@@ -7,17 +7,12 @@ public static class LoadService
 {
     public static async Task<List<Scratchcard>> LoadScratchcardsAsync()
     {
-        var options = new JsonSerializerOptions
-        {
-            PropertyNameCaseInsensitive = true,
-        };
-
         var scratchcards = new List<Scratchcard>();
 
         foreach (var filePath in Directory.EnumerateFiles("Scratchcards"))
         {
             var json = await File.ReadAllTextAsync(filePath);
-            scratchcards.Add(JsonSerializer.Deserialize<Scratchcard>(json, options)!);
+            scratchcards.Add(JsonSerializer.Deserialize<Scratchcard>(json)!);
         }
 
         return scratchcards;
