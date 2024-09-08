397e3ff2b4b6017e77ac5fed88bcbaebc1371178
Márk Ángyán <angyanmark97@gmail.com>
2024-09-08 16:47:01 +0200

    add HttpClient as scoped

--- src/Program.cs
+++ src/Program.cs
@@ -15,7 +15,7 @@ builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services
-    .AddSingleton(sp => new HttpClient { BaseAddress = new(builder.HostEnvironment.BaseAddress) })
+    .AddScoped(sp => new HttpClient { BaseAddress = new(builder.HostEnvironment.BaseAddress) })
     .AddFluentUIComponents();
 
 await builder.Build().RunAsync();
